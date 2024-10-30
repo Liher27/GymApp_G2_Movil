5934aed10018486c0cf8887f81766ccf55b55996
Liher27 <liher.chamorropa@elorrieta-errekamari.com>
2024-10-30 20:29:20 +0100

    vista de workouts actualizada

--- app/src/main/java/com/example/gymappxml/WorkoutsActivity.kt
+++ app/src/main/java/com/example/gymappxml/WorkoutsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.gymappxml
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
@@ -12,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 import pojo.Exercise
+import pojo.Workout
 
 
 class WorkoutsActivity : AppCompatActivity() {
@@ -22,40 +25,48 @@ class WorkoutsActivity : AppCompatActivity() {
     private lateinit var workoutsListView: ListView
     private lateinit var exerciseListView: ListView
 
-    private lateinit var workoutsList: List<String>
+    private lateinit var trainerButton: Button
+
+    private lateinit var workoutsList: List<Workout>
+    private lateinit var workoutsAdapter: ArrayAdapter<String>
     private lateinit var exerciseList: List<Exercise>
+
+    private var workoutLevel: Int = 0
+    private lateinit var workoutName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workouts)
 
+        trainerButton = findViewById<Button>(R.id.trainerButton)
         keyid = intent.getStringExtra("id") ?: "default_value"
         showLevel = findViewById(R.id.textView2)
 
         getUserLevel()
         loadWorkouts()
 
-        findViewById<Button>(R.id.trainerButton).setOnClickListener {
-            val intent = Intent(this@WorkoutsActivity, TrainerActivity::class.java).apply {
-                putExtra("id", keyid)
+        if (userIsTrainer()) {
+            trainerButton.setOnClickListener {
+                val intent = Intent(this@WorkoutsActivity, TrainerActivity::class.java).apply {
+                    putExtra("id", keyid)
+                }
+                startActivity(intent)
+                finish()
             }
-            startActivity(intent)
-            finish()
+        } else {
+            trainerButton.setVisibility(View.GONE);
         }
 
         findViewById<Button>(R.id.workoutBackBtn).setOnClickListener {
-            val intent = Intent(this@WorkoutsActivity, LoginActivity::class.java).apply {
-            }
-            startActivity(intent)
-            finish()
+            loadExercises("workout_0")
         }
         findViewById<Button>(R.id.workoutFilteringBtn).setOnClickListener {
-        if (filterText.text.toString().isEmpty()) {
-            Toast.makeText(this, "No has seleccionado ningún nivel", Toast.LENGTH_SHORT).show()
-        }else{
-            val level = filterText.text.toString().toInt()
-            filterWorkouts(level)
-        }
+            if (filterText.text.toString().isEmpty()) {
+                Toast.makeText(this, "No has seleccionado ningún nivel", Toast.LENGTH_SHORT).show()
+            } else {
+                val level = filterText.text.toString().toInt()
+                filterWorkouts(level)
+            }
 
         }
 
@@ -68,8 +79,8 @@ class WorkoutsActivity : AppCompatActivity() {
         }
 
         workoutsListView.setOnItemClickListener { _, _, position, _ ->
-            val workoutName = workoutsListView.getItemAtPosition(position).toString()
-            loadExercises(workoutName)
+            val selectedWorkout = workoutsList[position]
+            loadExercises(selectedWorkout.toString())
         }
     }
 
@@ -80,29 +91,44 @@ class WorkoutsActivity : AppCompatActivity() {
     private fun loadWorkouts() {
         val db = Firebase.firestore
         val userId = intent.getStringExtra("id")
+        val workoutsNames = mutableListOf<String>()
+
         workoutsListView = findViewById(R.id.workoutList)
         workoutsList = mutableListOf()
+
         userId?.let { id ->
             db.collection("users").document(id).collection("userHistory_0").get()
                 .addOnSuccessListener { result ->
                     for (document in result) {
+                        val workoutId = document.id
 
                         val workoutNameRef =
                             document.getDocumentReference("workoutName")
+
                         val workoutLevelRef =
                             document.getDocumentReference("workoutLvl")
+
                         workoutNameRef?.get()?.addOnSuccessListener { workoutDocument ->
-                            val workoutName =
-                                workoutDocument.getString("workoutName")
-                            (workoutsList as MutableList<String>).add(workoutName ?: "")
-                            val adapter =
-                                ArrayAdapter(
-                                    this,
-                                    android.R.layout.simple_list_item_1,
-                                    workoutsList
-                                )
-                            workoutsListView.adapter = adapter
+                            workoutName =
+                                workoutDocument.getString("workoutName")!!
                         }
+
+                        workoutLevelRef?.get()?.addOnSuccessListener { levelDocument ->
+                            workoutLevel =
+                                levelDocument.getLong("workoutLvl")!!.toInt()
+                        }
+
+                        val workout = Workout(workoutName, workoutLevel, workoutId)
+                        workoutsNames.add(workoutName ?: "")
+                        (workoutsList as MutableList<Workout>).add(workout)
+
+                        //workoutsAdapter = ArrayAdapter(
+                        //   this,
+                        //   android.R.layout.simple_list_item_1,
+                        //  workoutsList.get(0).toString()
+                        //)
+                        workoutsListView.adapter = workoutsAdapter
+
                     }
                 }
         }
@@ -110,13 +136,12 @@ class WorkoutsActivity : AppCompatActivity() {
 
     private fun loadExercises(workoutName: String) {
         val db = Firebase.firestore
-        exerciseListView = findViewById(R.id.workoutList)
+        exerciseListView = findViewById(R.id.exerciseView)
         exerciseList = mutableListOf()
 
-        db.collection("workouts").document(workoutName).collection("workoutExercises").get()
+        db.collection("workouts").document("workout_0").collection("workoutExercises").get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-
                     val exerciseName =
                         document.getString("exerciseName")
                     val image =
@@ -141,6 +166,7 @@ class WorkoutsActivity : AppCompatActivity() {
                     exerciseListView.adapter = adapter
                 }
             }
+
     }
 
     private fun getUserLevel() {
@@ -149,12 +175,27 @@ class WorkoutsActivity : AppCompatActivity() {
             db.collection("users").document(id).get().addOnSuccessListener { document ->
                 if (document != null) {
                     val level = document.getLong("userLevel")
-                    showLevel.text = "Nivel del usuario: $level"
+                    val levelText = "Nivel del usuario: $level"
+                    showLevel.text = levelText
                 }
             }.addOnFailureListener { exception ->
                 Log.e("showUserData", "Error getting user data: ", exception)
 
             }
         }
+    }
+
+    private fun userIsTrainer(): Boolean {
+        var ret = false
+        val db = Firebase.firestore
+        keyid.let { id ->
+            db.collection("users").document(id).get().addOnSuccessListener { document ->
+                if (document != null) {
+                    val isTrainer = document.getBoolean("trainer")
+                    ret = isTrainer ?: false
+                }
+            }
+        }
+        return ret
     }
 }
--- app/src/main/java/pojo/Workout.kt
+++ app/src/main/java/pojo/Workout.kt
@@ -3,6 +3,7 @@ package pojo
 data class Workout(
     val workoutName: String? = null,
     val workoutLevel: Int? = 0,
+    val workoutId: String? = null,
     val videoUrl: String? = null,
     val exerciseNumber: Int? = 0,
     val exerciseList: List<Exercise>? = null
